7e6a3eeea970ca84856c328d88c9a4b116c4cf04
Ali Rezaei <im1.bitcode@gmail.com>
2023-10-06 23:21:31 +0330

    Particles Container

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import ProjectsBtn from "@/components/ProjectsBtn";
 import Avatar from "@/components/Avatar";
 
 import { fadeIn } from "@/lib/variants-utils";
+import ParticlesContainer from "@/components/ParticlesContainer";
 
 export default function Home() {
   return (
@@ -53,15 +54,17 @@ export default function Home() {
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0" />
 
-        <div>Particles</div>
+        <div>
+          <ParticlesContainer />
+        </div>
 
         <motion.div
           variants={fadeIn("up", 0.5)}
           initial="hidden"
           animate="show"
           transition={{
-            duration: 1, 
-            ease: "easeInOut"
+            duration: 1,
+            ease: "easeInOut",
           }}
           exit="hidden"
           className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[6%]"
